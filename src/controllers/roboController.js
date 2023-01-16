ffb7fc00e469065ed0f362088c220e072bdf28e3
Gianine DataType <gianine.rosa@datatype.com.br>
2023-01-16 16:17:38 -0300

    Handling values and ordering by price

--- src/controllers/roboController.js
+++ src/controllers/roboController.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 module.exports = {
   async roboPesquisa(req, res){
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     const url = `https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops`;
     await page.goto(url);
@@ -13,11 +13,11 @@ module.exports = {
       const thumnArray = [...nodeList];
       const newElement = thumnArray.map( (item) => {
         let img = item.children[0].getAttribute('src');
-        let rating = item.children[2].children[1].getAttribute('data-rating');
-        let price = item.children[1].children[0].innerHTML;
+        let rating =  Number(item.children[2].children[1].getAttribute('data-rating'));
+        let price = Number(item.children[1].children[0].innerHTML.replace('$',''));
         let nameProduct = item.children[1].children[1].querySelector('.title').innerHTML;
         let description = item.children[1].querySelector('.description').innerHTML;
-        let review = item.children[2].children[0].innerHTML;
+        let review = Number(item.children[2].children[0].innerHTML.replace(' reviews', ''));
         
         return {
           img,
@@ -33,6 +33,16 @@ module.exports = {
 
     await browser.close();
 
-    return res.json(200,resultado);
+    const filtered = resultado.filter((item) => {
+      if(item.nameProduct.includes('Lenovo')){
+        return item;
+      }
+    })
+    
+    const resEnd = filtered.sort((a,b) => {
+      return a.price - b.price
+    })
+
+    return res.status(200).json(resEnd);
   },
 }
